92611309319472eaca4f438cceea0b603e3fa1a8
tdmadc107 <dangdat111@gmail.com>
2023-08-03 00:47:18 +0700

    setting structor

---
+++ src/main/java/com/manual/todoList/entity/User.java
@@ -0,0 +1,17 @@
+package com.manual.todoList.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+}
